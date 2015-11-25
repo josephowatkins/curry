878b8b463a1707b0e725fce6bb80b31ad5c72d00
Joe Watkins <joe.watkins@signal.uk.com>
2015-11-25 23:02:06 +0000

    Adds assoc/dissoc, updates tests

--- src/curry/utils.clj
+++ src/curry/utils.clj
@@ -1,5 +1,5 @@
 (ns curry.utils
-  (:refer-clojure :exclude [defn fn])
+  (:refer-clojure :exclude [defn fn = + - * / assoc dissoc])
   (:require [curry.core :refer :all]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -19,7 +19,7 @@
 
   clojure.lang.IPersistentMap
   (-fmap [this f]
-    (reduce (fn [acc [k v]] (assoc acc k (f v))) {} this))
+    (reduce (fn [acc [k v]] (clojure.core/assoc acc k (f v))) {} this))
 
   clojure.lang.LazySeq
   (-fmap [this f]
@@ -45,3 +45,130 @@
 
 (defn foldl [f v coll]
   (-foldl coll f v))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Maths - Binary operators
+
+(defn + [x y]
+  (clojure.core/+ x y))
+
+(defn - [x y]
+  (clojure.core/- x y))
+
+(defn * [x y]
+  (clojure.core/* x y))
+
+(defn / [x y]
+  (clojure.core// x y))
+
+(defn = [x y]
+  (clojure.core/= x y))
+
+(defn flip [f]
+  (fn [x y]
+    (f y x)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; prop, prop-or, prop-eq, path, path-or, path-eq
+
+(defprotocol Props
+  (-prop    [this prop])
+  (-prop-or [this default prop])
+  (-prop-eq [this prop value]))
+
+(extend-protocol Props
+  clojure.lang.IPersistentMap
+  (-prop [this prop]
+    (get this prop))
+  (-prop-or [this default prop]
+    (get this prop default))
+  (-prop-eq [this prop value]
+    (= (get this prop) value))
+
+  clojure.lang.IPersistentVector
+  (-prop [this prop]
+    (get this prop))
+  (-prop-or [this default prop]
+    (get this prop default))
+  (-prop-eq [this prop value]
+    (= (get this prop) value)))
+
+(defn prop [prop object]
+  (-prop object prop))
+
+(defn prop-or [default prop object]
+  (-prop-or object default prop))
+
+(defn prop-eq [prop value object]
+  (-prop-eq object prop value))
+
+(defprotocol Paths
+  (-path    [this path])
+  (-path-or [this default path])
+  (-path-eq [this path value]))
+
+(extend-protocol Paths
+  clojure.lang.IPersistentMap
+  (-path [this path]
+    (get-in this path))
+  (-path-or [this default path]
+    (get-in this path default))
+  (-path-eq [this path value]
+    (= (get-in this path) value))
+
+  clojure.lang.IPersistentVector
+  (-path [this path]
+    (get-in this path))
+  (-path-or [this default path]
+    (get-in this path default))
+  (-path-eq [this path value]
+    (= (get-in this path) value)))
+
+(defn path [path object]
+  (-path object path))
+
+(defn path-or [default path object]
+  (-path-or object default path))
+
+(defn path-eq [path value object]
+  (-path-eq object path value))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; assoc, assocPath, dissoc, dissocPath
+
+(defprotocol Associate
+  (-assoc      [this prop value])
+  (-assoc-path [this path value]))
+
+(extend-protocol Associate
+  clojure.lang.IPersistentMap
+  (-assoc [this prop value]
+    (clojure.core/assoc this prop value))
+  (-assoc-path [this path value]
+    (assoc-in this path value)))
+
+(defn assoc [prop value object]
+  (-assoc object prop value))
+
+(defn assoc-path [path value object]
+  (-assoc-path object path value))
+
+
+(defprotocol Dissociate
+  (-dissoc      [this prop])
+  (-dissoc-path [this path]))
+
+(declare dissoc)
+
+(extend-protocol Dissociate
+  clojure.lang.IPersistentMap
+  (-dissoc [this prop]
+    (clojure.core/dissoc this prop))
+  (-dissoc-path [this path]
+    (update-in this (vec (butlast path)) (dissoc (last path)))))
+
+(defn dissoc [prop object]
+  (-dissoc object prop))
+
+(defn dissoc-path [path object]
+  (-dissoc-path object path))
--- test/curry/utils_test.clj
+++ test/curry/utils_test.clj
@@ -1,21 +1,18 @@
 (ns curry.utils-test
   (:require [clojure.test :refer :all]
-            [curry.utils :refer :all]))
+            [curry.utils :as c]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Functor Law - fmap id = id
+;; fmap
 
 (deftest fmap-identity
   (testing "fmap id = id"
     (let [l '(1 2 3)
           v [1 2 3]
           m {:a 1 :b 2 :c 3}]
-      (is (= (fmap identity l) (identity l)))
-      (is (= (fmap identity v) (identity v)))
-      (is (= (fmap identity m) (identity m))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Functor Law - fmap (f . g) = fmap f . fmap g
+      (is (= (c/fmap identity l) (identity l)))
+      (is (= (c/fmap identity v) (identity v)))
+      (is (= (c/fmap identity m) (identity m))))))
 
 (deftest fmap-compose
   (testing "fmap (f . g) = fmap f . fmap g")
@@ -24,9 +21,122 @@
         l '(1 2 3)
         v [1 2 3]
         m {:a 1 :b 2 :c 3}]
-    (is (= (fmap (comp f g) l)
-           ((comp (fmap f) (fmap g)) l)))
-    (is (= (fmap (comp f g) v)
-           ((comp (fmap f) (fmap g)) v)))
-    (is (= (fmap (comp f g) m)
-           ((comp (fmap f) (fmap g)) m)))))
+    (is (= (c/fmap (comp f g) l)
+           ((comp (c/fmap f) (c/fmap g)) l)))
+    (is (= (c/fmap (comp f g) v)
+           ((comp (c/fmap f) (c/fmap g)) v)))
+    (is (= (c/fmap (comp f g) m)
+           ((comp (c/fmap f) (c/fmap g)) m)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Maths - Binary operators
+
+(deftest plus-test
+  (testing "+"
+    (let [plus-one (c/+ 1)]
+      (is (= 2 (plus-one 1))))))
+
+(deftest divide-test
+  (testing "/"
+    (let [divide (c// 3)]
+      (is (= 1 (divide 3))))))
+
+(deftest minus-test
+  (testing "-"
+    (let [minus (c/- 3)]
+      (is (= -2 (minus 5))))))
+
+(deftest multiply-test
+  (testing "*"
+    (let [multiply (c/* 10)]
+      (is (= 100 (multiply 10))))))
+
+(deftest flip-test
+  (testing "flip"
+    (let [minus-10 ((c/flip c/-) 10)
+          divide-by-7 ((c/flip c//) 7)]
+      (is (= 3 (minus-10  13)))
+      (is (= 2 (divide-by-7 14))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; prop, prop-or, prop-eq, path, path-or, path-eq
+
+(deftest prop-test
+  (testing "prop"
+    (let [m {:hello "world"}
+          prop-fn (c/prop :hello)] 
+      (is (= (prop-fn m) "world")))))
+
+(deftest prop-or-test
+  (testing "prop-or"
+    (let [m  {:a 5}
+          m' {:b 6}
+          prop-or-fn (c/prop-or :default :a)]
+      (is (= (prop-or-fn m) 5))
+      (is (= (prop-or-fn m') :default)))))
+
+(deftest prop-eq-test
+  (testing "prop-or"
+    (let [m  {:a 5}
+          m' {:a 6}
+          prop-eq-fn (c/prop-eq :a 5)]
+      (is (= (prop-eq-fn m) true))
+      (is (= (prop-eq-fn m') false)))))
+
+(deftest path-test
+  (testing "path"
+    (let [m {:a {"b" [1 2 3]}}
+          path-fn (c/path [:a "b" 0])]
+      (is (= (path-fn m) 1)))))
+
+(deftest path-or-test
+  (testing "path-or"
+    (let [m {:a {"b" [1 2 3]}}
+          m' {:a {"b" []}}
+          path-or-fn (c/path-or :default [:a "b" 0])]
+      (is (= (path-or-fn m) 1))
+      (is (= (path-or-fn m') :default)))))
+
+(deftest path-eq-test
+  (testing "path-eq"
+    (let [m  {:a {"b" 5}}
+          m' {:a 6}
+          path-eq-fn (c/path-eq [:a "b"] 5)]
+      (is (= (path-eq-fn m) true))
+      (is (= (path-eq-fn m') false)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; assoc, assoc-path, dissoc, dissoc-path
+
+(deftest assoc-test
+  (testing "assoc"
+    (let [m {:a 1 :b 2}
+          expected {:a 1 :b 2 :c 3}
+          assoc-fn (c/assoc :c 3) 
+          actual (assoc-fn m)]
+      (is (= actual expected)))))
+
+(deftest assoc-path-test
+  (testing "assoc-path"
+    (let [m {:a 1 :b {:c {:d 2}}}
+          expected {:a 1 :b {:c {:d 2 :e 3}}}
+          assoc-path-fn (c/assoc-path [:b :c :e] 3)
+          actual (assoc-path-fn m)]
+      (is (= actual expected)))))
+
+(deftest dissoc-test
+  (testing "dissoc"
+    (let [m {:a 1 :b 2}
+          expected {:a 1}
+          dissoc-fn (c/dissoc :b)
+          actual (dissoc-fn m)]
+      (is (= actual expected)))))
+
+(deftest dissoc-path-test
+  (testing "dissoc-path"
+    (let [m {:a 1 :b {:c {:d 2}}}
+          expected {:a 1 :b {:c {}}}
+          dissoc-path-fn (c/dissoc-path [:b :c :d])
+          actual (dissoc-path-fn m)]
+      (is (= actual expected)))))
+
