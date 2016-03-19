747407235c326f28c3b9d46b0ae6650839699070
Joe Watkins <joe.watkins@signal.uk.com>
2016-03-19 13:39:34 +0000

    Fix code layout

--- src/curry/core.clj
+++ src/curry/core.clj
@@ -2,8 +2,7 @@
   (:refer-clojure :exclude [defn fn]))
 
 (clojure.core/defn inits [xs]
-  (for [n (range (count xs))]
-    (take n xs)))
+  (for [n (range (count xs))] (take n xs)))
 
 (clojure.core/defn tails [xs]
   (map vec (take (inc (count xs)) (iterate rest xs))))
@@ -15,14 +14,17 @@
 (clojure.core/defn build-arity
   [fn-name [args remaining]]
   (let [next (rest (inits remaining))]
-    `([~@args] (clojure.core/fn ~@(for [args2 next] `([~@args2] (~fn-name ~@args ~@args2))) ([~@remaining] (~fn-name ~@args ~@remaining))))))
+    `([~@args]
+      (clojure.core/fn ~@(for [args2 next] `([~@args2] (~fn-name ~@args ~@args2)))
+        ([~@remaining] (~fn-name ~@args ~@remaining))))))
 
 (defmacro fn
   [binders & body]
   (when-not (vector? binders)
     (throw (IllegalArgumentException. "Second argument to defn must be in a vector")))
   (let [fn-name (gensym "curried")]
-    `(clojure.core/fn ~fn-name ~@(map (partial build-arity fn-name) (calculate-arity binders)) ([~@binders] ~@body))))
+    `(clojure.core/fn ~fn-name ~@(map (partial build-arity fn-name) (calculate-arity binders))
+       ([~@binders] ~@body))))
 
 (defmacro defn [name binders & body]
   (when-not (instance? clojure.lang.Symbol name)
